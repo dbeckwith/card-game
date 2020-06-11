cddc94c8ba17f507cb65d1d0283d1634e530d564
Anthony Beckwith <abeckwith@concordcarlisle.org>
2020-06-11 17:40:26 -0400

    added front end code to folder and renamed poker.html to index.html

---
+++ public/poker.js
@@ -0,0 +1,439 @@
+var suits = ["S", "D", "C", "H"];
+var values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"];
+var names = ["Jason", "Eve", "Patrick", "Sarah", "Wati", "Paul", "Ned", "Philecia", "James", "Andrew"];
+
+var players = [];
+var start_player = 0; //player that receives first card and bets first
+var current_player = 0;
+var current_card_up = true;
+var num_players = players.length; //tracks how many players didn't opt out
+var common_cards = [];
+var pot = 40;
+//SELECT MENUS FOR BETTING:
+//var menu1 = '<select onchange="bet(this)" name="bet" id="bet"' //allows for class for last player for slide fx
+//var menu2 =
+//    '<option value="b">BET</option>' +
+//    '<option value="chk">===</option>' +
+//    '<option value="chk">Check</option>' +
+//    '<option value="c1">1</option>' +
+//    '<option value="c2">2</option>' +
+//    '<option value="c3">3</option>' +
+//    '<option value="c4">4</option>' +
+//    '<option value="c5">5</option>' +
+//    '<option value="c6">6</option>' +
+//    '<option value="c7">7</option>' +
+//    '<option value="c8">8</option>' +
+//    '<option value="c9">9</option>' +
+//    '<option value="c10">10</option>' +
+//    '<option value="chalf">1/2 POT</option>' +
+//
+//    '<option value="cpot">POT</option>' +
+//
+//    '<option value="cother">Other</option>' +
+//    '</select>'
+
+
+function set_bet(player)
+{
+    //called when Bet button pressed
+}
+
+function payout()
+{
+    //divide pot by all winners
+}
+
+function add(p)
+{
+    //add a card in draw poker
+}
+function draw(){
+    //called when Draw button clicked - puts in draw mode where
+    //clicking card removes it
+    //remember: user sees all up with border around down
+}
+
+function bet(a)
+{
+    var x = (a.value || a.options[a.selectedIndex].value); //crossbrowser solution =)
+    if (x == "cother")
+        alert("Okay, how much do you want to bet?");
+    else if (x == "chalf")
+        alert("You have bet half the pot, which is " + pot / 2 + " chips");
+    else if (x == "cpot")
+        alert("You have bet the whole pot, which is " + pot + " chips");
+    else if (x == "check")
+        alert("You have checked");
+    else
+        alert("You have bet " + x.slice(1) + " chips ");
+}
+
+/**
+ * create player objects
+ */
+function setup()
+{
+    for (var i = 0; i < names.length; i++)
+    {
+        players.push(
+        {
+            name: names[i],
+            hand: [],
+            chips: 25,
+            fold: false,
+            playing: true
+        });
+    }
+    shuffle_restart();
+}
+
+function reset_dealer()
+{
+    start_player = 0;
+    shuffle_restart();
+}
+/**
+ * returns an array of 52 card objects with value, suit, up or down
+ */
+function getDeck()
+{
+    var deck = new Array();
+
+    for (var i = 0; i < suits.length; i++)
+    {
+        for (var x = 0; x < values.length; x++)
+        {
+            var card = {
+                value: values[x],
+                suit: suits[i],
+                up: false
+            };
+            deck.push(card);
+        }
+    }
+    return deck;
+}
+
+
+function shuffle_fisher_yates(deck)
+{
+    num_players = players.length;
+    var i = 0,
+        j = 0,
+        temp = null
+
+    for (i = deck.length - 1; i > 0; i -= 1)
+    {
+        j = Math.floor(Math.random() * (i + 1))
+        temp = deck[i]
+        deck[i] = deck[j]
+        deck[j] = temp
+    }
+    return deck;
+}
+
+
+function get_next_card()
+{
+    return deck.pop();
+}
+
+/**
+ * reset deck, for shuffle/restart
+ */
+function turn_in_cards()
+{
+    for (i = 0; i < players.length; i++)
+    {
+        players[i].hand = [];
+        players[i].fold = false;
+        players[i].playing = true;
+    }
+}
+//function rotate_deal(){
+//    players.push(players.shift());
+//}
+/**
+ * shuffle and reset everything
+ */
+function shuffle_restart()
+{
+    game_started = false;
+    common_cards = [];
+    document.getElementById("numcards").style.backgroundColor = "transparent";
+
+    deck = getDeck();
+    shuffle_fisher_yates(deck);
+    turn_in_cards();
+    start_player = start_player + 1;
+    current_player = start_player;
+    update_display(false);
+}
+/**
+ * flip the card from face to back or vice versa
+ */
+function flip(i, j)
+{
+    cd = players[i].hand[j];
+    cd.up = !cd.up;
+    update_display(false);
+}
+
+function flip_common(i)
+{
+    cd = common_cards[i];
+    cd.up = !cd.up;
+    update_display(false);
+}
+
+/**
+ * Deal a single card
+ * @param up_card boolean up or down card
+ */
+function one_card(up_card)
+{
+    dealt = false;
+
+    while (!dealt)
+    {
+        //not folded and isn't sitting out:
+        if (!players[current_player].fold && players[current_player].playing)
+        {
+            dealt = true;
+            card = get_next_card();
+            if (typeof card !== 'undefined')
+            {
+                if (!up_card)
+                    card.up = false;
+                else
+                    card.up = true;
+
+                player = players[current_player]
+                player.hand.push(card);
+
+                update_display(true);
+
+                current_player++;
+                current_player %= players.length;
+            }
+        }
+        else //try next player
+        {
+            current_player++;
+            current_player %= players.length;
+        }
+    }
+}
+/**
+ * deals card; used in all multi-player deals (1UpAll, etc.)
+ */
+function deal_card(up_card)
+{
+    card = get_next_card();
+    if (typeof card !== 'undefined')
+    {
+        if (!up_card)
+            card.up = false;
+        else
+            card.up = true;
+
+        //get current player and add card to their hand:
+        player = players[current_player]
+        player.hand.push(card);
+
+        update_display(true);
+
+        //move to next player
+        current_player++;
+        current_player %= players.length;
+    }
+}
+/**
+ * check if player in and still players left to deal to, then deal card
+ */
+function check_and_deal_card(count, up)
+{
+    if (!players[current_player].fold && players[current_player].playing && count < num_players)
+    {
+        deal_card(up);
+        return 1;
+    }
+    else //move to next player:
+    {
+        current_player++;
+        current_player %= players.length;
+        return 0;
+    }
+}
+/**
+ * Deals specified number of down and up cards to all active players
+ */
+function deal_all(down, up)
+{
+    game_started = true;
+    current_player = start_player;
+
+    //downs:
+    for (var d = 0; d < down; d++)
+    {
+        count = 0;
+        for (var i = 0; i < players.length; i++)
+        {
+            count += check_and_deal_card(count, false);
+        }
+    }
+    //ups:
+    for (var u = 0; u < up; u++)
+    {
+        count = 0;
+        for (var i = 0; i < players.length; i++)
+        {
+            count += check_and_deal_card(count, true);
+        }
+    }
+}
+
+
+function fold(p)
+{
+    players[p].fold = true;
+    //turn all cards over:
+    for (var j = 0; j < players[p].hand.length; j++)
+        players[p].hand[j].up = false;
+
+    update_display(false);
+
+    num_players -= 1;
+}
+
+function sitout(p)
+{
+    if (!game_started)
+    {
+        players[p].playing = false;
+        num_players -= 1;
+        update_display(false);
+    }
+}
+
+function deal_common()
+{
+    card = get_next_card();
+    if (typeof card !== 'undefined')
+    {
+        common_cards.push(card);
+        common_disp = "";
+        for (var i = 0; i < common_cards.length; i++)
+            common_disp += "<img class='card' onclick='flip_common(" + i + ")'  src='card_images/1B.svg'>";
+
+        update_display(false);
+
+        document.getElementById("common").innerHTML = common_disp;
+    }
+}
+
+/**
+ * updates all cards on field
+ */
+function update_display(slide)
+{
+    document.getElementById("numcards").innerHTML = "remaining cards: " + deck.length;
+    if (deck.length == 0)
+        document.getElementById("numcards").style.backgroundColor = "red";
+
+    display = "<table>";
+
+
+    //two columns of players, 5 per column:
+    for (i = 0; i < Math.ceil(players.length / 2); i++)
+    {
+        for (col = 0; col < 2; col++)
+        {
+            loc = i + 5 * col; //loc in second column
+
+            if (loc < players.length)
+            {
+                if (loc < 5)
+                    display += "<tr><td>"; //first column, so start new row
+                else
+                    display += "<td>"; //second column, so start new data
+
+                //BET select menu:
+                //                display += menu1 + loc + "'>" + menu2
+
+                //2 buttons and the name:
+                if (!game_started)
+                    display += "<button id='fold' type='button' onclick='sitout(" + loc + ")'>" +
+                    "SIT OUT</button>"
+                if (loc == start_player - 1)
+                    clr_add = "style='color:yellow'";
+                else if (loc == current_player + 1)
+                    clr_add = "style='color:red'";
+                else
+                    clr_add = ""
+                display +=
+
+                    "<button id='fold' type='button' onclick='set_bet(" + loc + ")'>Bet</button>" +
+
+                    "<button  class='dlrbtns' type='button' onclick='add(" + loc + ")'>" + "Add</button>" +
+
+                   
+                    "<button class='dlrbtns' type='button' onclick='won(" + loc + ")'>Won</button><br>" +
+
+
+                    '<input type="number" size="4" maxlength="4" id="bet' + loc + '" name="fname" style="width:50px;" min="0" max="999">' +
+
+                    "<button id='fold' type='button' onclick='fold(" + loc + ")'>" + "FOLD</button>" +
+ "<span onclick=give_card(" + loc + ")" + clr_add + ">" + players[loc].name + "</span><br>";
+
+                display += "</td><td>";
+
+                //the cards:
+                for (j = 0; j < players[loc].hand.length; j++)
+                {
+                    //last card only should show animation:
+                    if (loc == current_player && j == players[i].hand.length - 1 && slide)
+                        add = " id='last' ";
+                    else
+                        add = "";
+
+                    curr_card = players[loc].hand[j];
+
+                    //out of game - grey back of card:
+                    if (players[loc].fold || !players[loc].playing)
+                        display += "<img class='card' onclick='flip()' src='card_images/1B.svg'>";
+                    //in play - up card
+                    else if (curr_card.up)
+                        display += "<img " + add + "class='card' onclick='flip(" + loc + ", " + j + ")' src='card_images/" +
+                        curr_card.value + curr_card.suit +
+                        ".svg'>";
+                    //in play - down card:
+                    else
+                        display += "<img " + add + "class='card' onclick='flip(" + loc + ", " + j + ")' src='card_images/2B.svg'>";
+                }
+                if (loc < 5)
+                    display += "</td>";
+                else
+                    display += "</td></tr>";
+            }
+        }
+    }
+
+    display += "</table>COMMON:";
+
+    document.getElementById("field").innerHTML = display;
+
+    //common cards:
+    common_disp = "";
+    for (var i = 0; i < common_cards.length; i++)
+    {
+        if (common_cards[i].up)
+            common_disp += "<img class='card' onclick='flip_common(" + i + ")' src='card_images/" + common_cards[i].value +
+            common_cards[i].suit + ".svg'>";
+        else
+            common_disp += "<img class='card' onclick='flip_common(" + i + ")'  src='card_images/1B.svg'>";
+    }
+    document.getElementById("common").innerHTML = common_disp;
+
+
+}
