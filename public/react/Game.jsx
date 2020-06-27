78d95a5fb07bd37f0015f917e04ac6f7fbb70c73
Daniel Beckwith <djbsnx@gmail.com>
2020-06-27 19:03:49 -0400

    Add basic player seat rendering

--- public/react/Game.jsx
+++ public/react/Game.jsx
@@ -1,5 +1,27 @@
 import { gameClient } from './GameClient';
-import { VBox } from './Box';
+import { HBox, VBox } from './Box';
+
+const CardImage = styled.img`
+  height: 100px;
+  object-fit: contain;
+`;
+
+const PlayerCard = ({ player, card, up }) => {
+  return (
+    <CardImage src={`/card_images/${card}.svg`} />
+  );
+};
+
+const PlayerSeat = ({ player }) => {
+  return (
+    <HBox>
+      <span>{player.name}</span>
+      <HBox>
+        {_.map(player.hand, ({ card, up }, idx) => <PlayerCard key={idx} player={player} card={card} up={up} />)}
+      </HBox>
+    </HBox>
+  );
+};
 
 export const Game = ({ gameState }) => {
   return (
@@ -12,6 +34,9 @@ export const Game = ({ gameState }) => {
       >
         Log Out
       </button>
+      <VBox>
+        {_.map(gameState.players, (player, idx) => <PlayerSeat key={idx} player={player} />)}
+      </VBox>
     </VBox>
   );
 };
